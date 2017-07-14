b5d43acedab08dae573f5769d062a6406e14beb6
Renan Kummer <renankj@gmail.com>
2017-07-14 12:32:51 -0300

    Initial commit
    Decides which main() function must be executed.

---
+++ Tests/MainControl.cpp
@@ -0,0 +1,14 @@
+/**
+ * Chooses which main function must be called.
+ *
+ *  Created on: July 7th, 2017
+ *     Authors: Aline Weber
+ *              Renan Kummer
+ */
+
+/**
+ * ONLY ONE MACRO MUST BE DEFINED AT A TIME FOR THE PROGRAM TO COMPILE!
+ */
+
+//#define MAIN_
+#define STRINGHASHDATA_TEST_
